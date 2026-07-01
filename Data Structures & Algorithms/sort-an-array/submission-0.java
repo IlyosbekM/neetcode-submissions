9e4967ce3fc4bd62242ff0da3deb39f6105b2d66
Ilyosbek <100830887+IlyosbekM@users.noreply.github.com>
2026-07-01 16:19:13 +0500

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] sortArray(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no elements were swapped in the inner loop, array is sorted
+            if (!swapped)
+                break;
+        }
+
+        return arr;
+    }
+}
